5775a8c8bcf3b098ab6579cdb177df1ea5268b79
khmmamed <khmammed@gmail.com>
2018-09-01 18:31:04 +0100

    update

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 const 	express		= require('express'),
 		router  	= express.Router(),
 		babelify   	= require('express-babelify-middleware'),
-        cssify      = require('cssify'),
 		//lab     	= require('../Core/lab'),
         passport    = require('passport'),
 		User 		= require('./member/login'),
@@ -15,14 +14,14 @@ const 	express		= require('express'),
     /**Pages**/
 router.get(     '/'               , (req, res, next)=>{ res.render('Layout.js',{title: 'eLab', page:'MedApp - Home', script: '/index.js'}) });
 router.get(     '/userMenu'       , (req, res, next)=>{ req.user ? res.send({user : req.user }) : res.send({user : '' } ) })
-router.get(     '/index.js'       , (req, res, next)=>{ next()},babelify('views/main.js',{minify: true, transform:[cssify]}))
+router.get(     '/index.js'       , (req, res, next)=>{ next()},babelify('views/main.js',{minify: true}))
 router.get(     '/eLab'           , (req, res, next)=>{ res.render('Layout.js',{title: 'eLab', page:'eLab home page', script: '/eLab.js'}) });
-router.get(     '/eLab.js'        , (req, res, next)=>{ next() },babelify('views/frontApi/eLab/eLab.js',{minify: true, transform:[cssify]}));
+router.get(     '/eLab.js'        , (req, res, next)=>{ next() },babelify('views/frontApi/eLab/eLab.js',{minify: true}));
 router.get(     '/Login'          , (req, res)=>{ res.render('Layout.js',{script: '/Login.js'})});
 router.post(    '/users'          , passport.authenticate('local-login'), (req, res)=>{ res.send({msg : 'success'}) });
 router.get(     '/success'        , (req, res)=>{res.send('success')});
 router.get(     '/failure'        , (req, res)=>{res.send('failure')});
-router.get(     '/Login.js'       , babelify('views/Login.js',{minify: true, transform:[cssify]}),(req, res)=>{console.log('work')});
+router.get(     '/Login.js'       , babelify('views/Login.js',{minify: true }),(req, res)=>{console.log('work')});
 router.post(    '/Login'          , (req,res)=>{console.log(req.body)});
 
     /** API **/
@@ -33,7 +32,7 @@ router.get(     '/eLab/tests'     , (res,req,next)=>{ next() } ,Lab.autoComplete
 
 
 //Admin Area
-router.get(     '/admlab.js'     ,  babelify('views/adminApi/AdmLab.js',{minify: true, transform:[cssify]}));
+router.get(     '/admlab.js'     ,  babelify('views/adminApi/AdmLab.js',{minify: true }));
 router.get(     '/:user'         ,  (req, res, next) => { res.render('Layout.js', {title: 'Administration', page : 'Admin Home page', script : '/admlab.js'}); next(); });
 
 //Organism Area
